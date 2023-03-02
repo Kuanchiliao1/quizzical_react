05b56b3fe7acff28cfc0dc18379dae70ed580c31
kuanchiliao1 <kuanchiliao@gmail.com>
2023-03-02 16:39:41 -0500

    Add the start page

---
+++ quizzical_react/src/StartPage.jsx
@@ -0,0 +1,11 @@
+import "./index.css"
+
+export default function StartPage() {
+  return (
+    <div className="start-page">
+      <h1 className="title">Quizzical</h1>
+      <p>Enter a topic or Start Quiz for random topic</p>
+      <button>Start quiz</button>
+    </div>
+  )
+}
