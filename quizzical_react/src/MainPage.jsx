a26e65280c18a6f0661e203a1c3c66ee10ff3a5f
kuanchiliao1 <kuanchiliao@gmail.com>
2023-03-06 17:59:24 -0500

    Add most form functionality

--- quizzical_react/src/MainPage.jsx
+++ quizzical_react/src/MainPage.jsx
@@ -2,17 +2,7 @@ import "./index.css"
 import React from 'react'
 
 export default function MainPage(props) {
-  // question: checked?, correct answer, user answer
-  const [answers, setAnswers] = React.useState([
-    {
-
-    }
-  ])
-
-  // New problem: form with ONLY 4 selections
-  // Choices: A B C D
-    // User choose C but the answer is D
-  const [formData, setFormData] = React.useState(
+  const [allFormData, setAllFormData] = React.useState(
       props.questions.reduce((object, questionData, index) => {
         let formId = index + 1
         object[formId] = {
@@ -22,85 +12,87 @@ export default function MainPage(props) {
         return object
       }, {})
     )
-  console.log(formData)
+  console.log(allFormData)
 
   function handleEvent(event) {
     const {value, dataset} = event.target
     const formId = dataset.formId
-    setFormData(function(oldFormData) {
+    setAllFormData(function(oldAllFormData) {
       return {
-        ...oldFormData,
+        ...oldAllFormData,
         [formId]: {
-          correct: oldFormData[formId].correct,
+          correct: oldAllFormData[formId].correct,
           checkedChoice: value
         },
       }
     })
   }
 
-  // setFormData(function(oldForm) {
-  //   event.target
-  //   return {
-  //     ...oldForm,
-
-      
-  //   }
-  // })
-
   // returns 1-4 to represent correct choice
   function findCorrectChoice(questionData) {
     return questionData.choices.findIndex(choice => choice.correct) + 1
   }
 
-
 /*
   ***Problem***
-    - Description:
-    - Input: varying number of multiple choice questions
-      - 4 options each
-    - Output: the incorrect guess made by the user and the correct answer
+    - Description: When button is clicked, check if all questions have an answer. Only run function if so.
+      - If so, then dynamically add "correct" classes to all the questions. Disable the button and any further input somehow.
+      - If not, do nothing
+    - Input: 
+      -
+    - Output:
       -
     - Questions
-      - How do I uniquely identify each question? => with index + form id for the question
 
   ***Data Structure***
-    - its an array of objects
-      - each object represents question - questionId {correct: 1-4, checked: 1-4}
-    - input attributes - name: choice, value: 1-4, class: choice-#
+    -
+
   ***Algorithm***
-    - Iterate through 10 question objects
-      -
-      - 
-    - onChange: when the form value changes
-      - Set the state to reflect the change
-        - 
-      - use that state to set the value in the form
+    -
 
   */
 
+  function areFormsAllFilled() {
+    console.log(Object.values(allFormData))
+    return Object.values(allFormData).every(form => form.checkedChoice )
+  }
+
+  function submitAllForms() {
+    if (areFormsAllFilled()) {
+      console.log("yep!")
+    } else {
+      console.log("nope!")
+    }
+  }
+
+  // Is this a good task for useEffect?
+  React.useEffect(function() {
+    console.log("effect")
+    questionElements.forEach(formEl => {
+    })
+  }, [])
 
   const questionElements = props.questions.map((questionData, index) => {
     const {question, choices} = questionData
     const formId = index + 1
+    const selectedValue = allFormData[formId].checkedChoice
+    const formInputs = []
+    
+    for(let i = 0; i < choices.length; i++ ) {
+      const value = String(i + 1)
+
+      formInputs.push(
+        <label>
+          <input onChange={handleEvent} type="radio" name="choice" value={value} data-form-id={formId} checked={selectedValue === value ? true : false} />
+          {choices[i].text}
+        </label>
+      )
+    }
+
     return (
       <form id={formId} className="question-container">
         <h2>{question}</h2>
-        <label>
-          <input onClick={handleEvent} type="radio" name="choice" value="1" data-form-id={formId} id={`q${formId}choice1`} />
-          {choices[0].text}
-        </label>
-        <label>
-          <input onClick={handleEvent} type="radio" name="choice" value="2" data-form-id={formId} />
-          {choices[1].text}
-        </label>
-        <label>
-          <input onClick={handleEvent} type="radio" name="choice" value="3" data-form-id={formId} />
-          {choices[2].text}
-        </label>
-        <label>
-          <input onClick={handleEvent} type="radio" name="choice" value="4" data-form-id={formId} />
-          {choices[3].text}
-        </label>
+        {formInputs}
       </form>
     )
   })
@@ -109,6 +101,7 @@ export default function MainPage(props) {
     <div>
       {questionElements}
       <h1></h1>
+      <button onClick={submitAllForms}>Submit</button>
     </div>
   )
 }
