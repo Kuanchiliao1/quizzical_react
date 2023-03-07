c8d5e541507d353df131f98e9aa7e201736a0ef3
kuanchiliao1 <kuanchiliao@gmail.com>
2023-03-07 12:37:21 -0500

    Refactor for loop into map

--- quizzical_react/src/MainPage.jsx
+++ quizzical_react/src/MainPage.jsx
@@ -1,82 +1,92 @@
-import "./index.css"
-import React from 'react'
-import './index.css'
+import "./index.css";
+import React from "react";
+import "./index.css";
 
 export default function MainPage(props) {
   const [allFormData, setAllFormData] = React.useState(
-      props.questions.reduce((object, questionData, index) => {
-        let formId = index + 1
-        object[formId] = {
-          correct: findCorrectChoice(questionData).toString(),
-          checkedChoice: 0
-        }
-        return object
-      }, {})
-    )
-  
-  const [isQuizSubmitted, setIsQuizSubmitted] = React.useState(false)
+    props.questions.reduce((object, questionData, index) => {
+      let formId = index + 1;
+      object[formId] = {
+        correct: findCorrectChoice(questionData).toString(),
+        checkedChoice: 0,
+      };
+      return object;
+    }, {})
+  );
+
+  const [isQuizSubmitted, setIsQuizSubmitted] = React.useState(false);
 
   function handleEvent(event) {
-    const {value, dataset} = event.target
-    const formId = dataset.formId
-    setAllFormData(function(oldAllFormData) {
+    const { value, dataset } = event.target;
+    const formId = dataset.formId;
+    setAllFormData(function (oldAllFormData) {
       return {
         ...oldAllFormData,
         [formId]: {
           correct: oldAllFormData[formId].correct,
-          checkedChoice: value
+          checkedChoice: value,
         },
-      }
-    })
+      };
+    });
   }
 
   // returns 1-4 to represent correct choice
   function findCorrectChoice(questionData) {
-    return questionData.choices.findIndex(choice => choice.correct) + 1
+    return questionData.choices.findIndex((choice) => choice.correct) + 1;
   }
 
   function areFormsAllFilled() {
-    console.log(Object.values(allFormData))
-    return Object.values(allFormData).every(form => form.checkedChoice )
+    console.log(Object.values(allFormData));
+    return Object.values(allFormData).every((form) => form.checkedChoice);
   }
 
   function submitAllForms() {
     if (areFormsAllFilled()) {
-      setIsQuizSubmitted(oldBoolean => !oldBoolean)
-      console.log(isQuizSubmitted)
+      setIsQuizSubmitted((oldBoolean) => !oldBoolean);
     } else {
-      alert("Please answer all questions!")
+      alert("Please answer all questions!");
     }
   }
 
+  function getScore() {
+    ""
+  }
+
   // Is this a good task for useEffect?
-  React.useEffect(function() {
-    console.log("effect")
-    questionElements.forEach(formEl => {
-    })
-  }, [])
+  React.useEffect(function () {
+    console.log("effect");
+    questionElements.forEach((formEl) => {});
+  }, []);
 
   const questionElements = props.questions.map((questionData, index) => {
-    const {question, choices} = questionData
-    const formId = (index + 1).toString()
-    const selectedChoice = allFormData[formId].checkedChoice
-    const correctChoice = allFormData[formId].correct
-    const formInputs = []
-    
-    for(let i = 0; i < choices.length; i++ ) {
-      const choiceId = (i + 1).toString()
-      const isSelected = selectedChoice === choiceId ? true : false
+    const { question, choices } = questionData;
+    const formId = (index + 1).toString();
+    const selectedChoice = allFormData[formId].checkedChoice;
+    const correctChoice = allFormData[formId].correct;
 
-      const correctClass = (isQuizSubmitted && (correctChoice === choiceId)) ? "show-correct-choice" : ""
-      const submittedClass = isQuizSubmitted ? "submitted" : ""
-      
-      formInputs.push(
+    const formInputs = choices.map((choice, index) => {
+      const choiceId = (index + 1).toString();
+      const isSelected = selectedChoice === choiceId ? true : false;
+
+      const correctClass =
+        isQuizSubmitted && correctChoice === choiceId
+          ? "show-correct-choice"
+          : "";
+      const submittedClass = isQuizSubmitted ? "submitted" : "";
+      return (
         <label className={`${correctClass} ${submittedClass}`}>
-          <input onChange={handleEvent} type="radio" name="choice" value={choiceId} data-form-id={formId} checked={isSelected} />
-          {choices[i].text}
+          <input
+            onChange={handleEvent}
+            type="radio"
+            name="choice"
+            value={choiceId}
+            data-form-id={formId}
+            checked={isSelected}
+          />
+          {choice.text}
         </label>
       )
-    }
+    })
 
     return (
       <>
@@ -85,14 +95,18 @@ export default function MainPage(props) {
           <div className="choices-container">{formInputs}</div>
         </form>
       </>
-    )
-  })
+    );
+  });
 
   return (
     <div className="questions-container">
       {questionElements}
       <h1></h1>
-      {isQuizSubmitted ? <p>Other thing</p> : <button onClick={submitAllForms}>Check answers</button>}
+      {isQuizSubmitted ? (
+        <button onClick={props.start}>Play again</button>
+      ) : (
+        <button onClick={submitAllForms}>Check answers</button>
+      )}
     </div>
-  )
+  );
 }
