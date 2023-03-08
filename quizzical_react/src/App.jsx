fa8647189ea4af7278187e33e949382b05435b18
kuanchiliao1 <kuanchiliao@gmail.com>
2023-03-08 13:57:13 -0500

    Format and refactor

--- quizzical_react/src/App.jsx
+++ quizzical_react/src/App.jsx
@@ -1,72 +1,82 @@
-import { useState } from 'react'
-import React from 'react'
-import './index.css'
-import StartPage from './StartPage'
-import MainPage from './MainPage'
+import { useState } from "react";
+import React from "react";
+import "./index.css";
+import StartPage from "./components/StartPage";
+import MainPage from "./components/MainPage";
+import getAIOutput from "./utils";
 
 function App() {
-  const [start, setStart] = React.useState(false)
-  const [questions, setQuestions] = React.useState([])
-  const [newQuestions, setNewQuestions] = React.useState([])
+  const [start, setStart] = React.useState(false);
+  const [questions, setQuestions] = React.useState([]);
+  const [newQuestions, setNewQuestions] = React.useState([]);
+  let useEffectCount = 0;
 
   function startQuiz() {
-    setStart(true)
+    setStart(true);
   }
 
   // fisher-yates shuffle
   function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1))
-      const temp = array[i]
-      array[i] = array[j]
-      array[j] = temp
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
     }
-    return array
+    return array;
   }
 
   function decodeBase64(string) {
-    string = atob(string)
-    string = string.replace(/Ã©/, "é")
-    return string
+    string = atob(string);
+    string = string.replace(/Ã©/, "é");
+    return string;
   }
 
   function fetchNewQuestions() {
-    setNewQuestions([])
-    setStart(false)
-    console.table(questions)
+    setNewQuestions([]);
+    setStart(false);
   }
 
   // Only run on initial render/rerender
-  React.useEffect(
-    () => {
-      fetch("https://opentdb.com/api.php?amount=3&type=multiple&encode=base64")
-        .then(res => res.json())
-        .then(data => {
-          const questionObjects = data.results.map(questionData => {
-            const {question, incorrect_answers, correct_answer} = questionData
+  React.useEffect(() => {
+    if (start) {
+      fetch("https://opentdb.com/api.php?amount=5&type=multiple&encode=base64")
+        .then((res) => res.json())
+        .then((data) => {
+          const questionObjects = data.results.map((questionData) => {
+            const { question, incorrect_answers, correct_answer } =
+              questionData;
             return {
               question: decodeBase64(question),
               choices: shuffle([
-                {text: decodeBase64(correct_answer), correct: true},
-                {text: decodeBase64(incorrect_answers[0]), correct: false},
-                {text: decodeBase64(incorrect_answers[1]), correct: false},
-                {text: decodeBase64(incorrect_answers[2]), correct: false}
-              ])
-            }
-          })
-          console.log("effect")
-          setQuestions(questionObjects)
-        })
-    }, [newQuestions])
+                { text: decodeBase64(correct_answer), correct: true },
+                { text: decodeBase64(incorrect_answers[0]), correct: false },
+                { text: decodeBase64(incorrect_answers[1]), correct: false },
+                { text: decodeBase64(incorrect_answers[2]), correct: false },
+              ]),
+            };
+          });
+          setQuestions(questionObjects);
+          console.count("quiz API");
+          getAIOutput();
+          useEffectCount += 1;
+        });
+    }
+  }, [newQuestions]);
 
   return (
     <div className="App">
-      {start ? <MainPage questions={questions} fetchQuestions={fetchNewQuestions} /> : <StartPage start={startQuiz} />}
+      {start ? (
+        <MainPage questions={questions} fetchQuestions={fetchNewQuestions} />
+      ) : (
+        <StartPage start={startQuiz} />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
 
 /*
 When someone presses the button, conditionally render the first and main page
