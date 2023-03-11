6f06d18db1dd92f75d76049686e35572e6410651
kuanchiliao1 <kuanchiliao@gmail.com>
2023-03-11 18:05:12 -0500

    Add "enter" keydown event to start quiz

--- quizzical_react/src/components/StartPage.jsx
+++ quizzical_react/src/components/StartPage.jsx
@@ -1,12 +1,32 @@
 import "../index.css"
 import React from "react";
+import { fetchAIScoreFeedback } from "../utils"
 
 export default function StartPage(props) {
   function handleInput(event) {
-    console.log(event.target.value)
     props.setCustomQuizTopic(oldInput => {
       return event.target.value
     })
+  }
+
+  function handleOnKeyDown(event) {
+    if (event.key === "Enter") {
+      props.start()
+    }
+  }
+
+  function handleResetBtn() {
+    props.setStoredQuizData(oldScore => {
+      return ({
+        ...oldScore,
+        questionsCorrect: 0,
+        questionsTotal: 0
+      })
+    })
+  }
+
+  function getScoreMessage() {
+    return fetchAIScoreFeedback()
   }
 
   return (
@@ -15,9 +35,11 @@ export default function StartPage(props) {
       <p className="start-description">
         Quiz on random topics or enter your own topic to generate a custom quiz!
         <span className="warning">⚠️ Warning: AI questions and explanations may be *extremely* inaccurate</span>
-        <input className="custom-quiz" placeholder="(optional) e.g. Star Wars" type="text" onChange={handleInput} value={props.customQuizTopic} />
+        <input className="custom-quiz" placeholder="(optional) e.g. Star Wars" type="text" onChange={handleInput} onKeyDown={handleOnKeyDown} value={props.customQuizTopic} />
       </p>
       <button className="start-quiz" onClick={props.start}>Start general quiz</button>
+      <button className="reset-score" onClick={handleResetBtn}>Reset Score</button>
+      <p className="score-message">{getScoreMessage()}</p>
     </div>
   )
 }
