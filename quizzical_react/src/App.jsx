2fa9b7865fbe2913a1f076a0bdb106f10cd441bd
kuanchiliao1 <kuanchiliao@gmail.com>
2023-03-11 18:03:36 -0500

    Set up state for question storage

--- quizzical_react/src/App.jsx
+++ quizzical_react/src/App.jsx
@@ -7,6 +7,11 @@ import MainPage from "./components/MainPage";
 function App() {
   const [start, setStart] = React.useState(false);
   const [customQuizTopic, setCustomQuizTopic] = React.useState("")
+  const [storedQuizData, setStoredQuizData] = React.useState({
+    questionsCorrect: 0,
+    questionsTotal: 0,
+    savedQuestions: []
+  })
 
   function startQuiz() {
     setStart(true);
@@ -16,12 +21,21 @@ function App() {
     setStart(false)
   }
 
+  React.useEffect(() => {
+    const data = window.localStorage.getItem("saved-quiz-information")
+    if ( data !== null) setStoredQuizData(JSON.parse(data))
+  }, [])
+
+  React.useEffect(() => {
+    window.localStorage.setItem("saved-quiz-information", JSON.stringify(storedQuizData))
+  }, [storedQuizData])
+
   return (
     <div className="App">
       {start ? (
-        <MainPage end={endQuiz} customQuizTopic={customQuizTopic} />
+        <MainPage end={endQuiz} customQuizTopic={customQuizTopic} setStoredQuizData={setStoredQuizData} />
       ) : (
-        <StartPage start={startQuiz} setCustomQuizTopic={setCustomQuizTopic} customQuizTopic={customQuizTopic}/>
+        <StartPage start={startQuiz} setStoredQuizData={setStoredQuizData} storedQuizData={storedQuizData} setCustomQuizTopic={setCustomQuizTopic} customQuizTopic={customQuizTopic}/>
       )}
     </div>
   );
