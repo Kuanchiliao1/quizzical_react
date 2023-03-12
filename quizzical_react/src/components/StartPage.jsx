332da5d9d23e22dbb2c823afedc1169a94050e9d
kuanchiliao1 <kuanchiliao@gmail.com>
2023-03-12 14:08:52 -0400

    Modify start page for saved questions

--- quizzical_react/src/components/StartPage.jsx
+++ quizzical_react/src/components/StartPage.jsx
@@ -1,8 +1,11 @@
 import "../index.css"
 import React from "react";
+import SavedQuestionsPage from "./SavedQuestionsPage.jsx"
 import { fetchAIScoreFeedback } from "../utils"
 
 export default function StartPage(props) {
+  const [isReset, setIsReset] = React.useState(null)
+
   function handleInput(event) {
     props.setCustomQuizTopic(oldInput => {
       return event.target.value
@@ -16,6 +19,7 @@ export default function StartPage(props) {
   }
 
   function handleResetBtn() {
+    setIsReset(true)
     props.setStoredQuizData(oldScore => {
       return ({
         ...oldScore,
@@ -25,21 +29,36 @@ export default function StartPage(props) {
     })
   }
 
+  function handleViewSavedBtn() {
+    props.setViewSaved((oldValue) => !oldValue)
+  }
+
   function getScoreMessage() {
-    return fetchAIScoreFeedback()
+    const {questionsCorrect, questionsTotal} = props.storedQuizData
+    if (questionsCorrect + questionsTotal === 0) {
+      return "No score yet"
+    } else {
+      return `Total Score: ${questionsCorrect} out of ${questionsTotal}`
+    }
   }
 
   return (
+    props.viewSaved ? <SavedQuestionsPage storedQuizData={props.storedQuizData} setStoredQuizData={props.setStoredQuizData} handleViewSavedBtn={handleViewSavedBtn}/> :
     <div className="start-page">
       <h1 className="title">🤖 Quizzical</h1>
       <p className="start-description">
         Quiz on random topics or enter your own topic to generate a custom quiz!
         <span className="warning">⚠️ Warning: AI questions and explanations may be *extremely* inaccurate</span>
-        <input className="custom-quiz" placeholder="(optional) e.g. Star Wars" type="text" onChange={handleInput} onKeyDown={handleOnKeyDown} value={props.customQuizTopic} />
       </p>
-      <button className="start-quiz" onClick={props.start}>Start general quiz</button>
-      <button className="reset-score" onClick={handleResetBtn}>Reset Score</button>
-      <p className="score-message">{getScoreMessage()}</p>
+      <div className="quiz-components-container">
+        <input className="custom-quiz" placeholder="(optional) e.g. Star Wars" type="text" onChange={handleInput} onKeyDown={handleOnKeyDown} value={props.customQuizTopic} />
+        <button className="start-quiz" onClick={props.start}>Start quiz!</button>
+        <p className="score-message">{getScoreMessage()}</p>
+        <div className="reset-save-btn-container">
+          <button disabled={isReset || getScoreMessage() === "No score yet"} className="reset-score" onClick={handleResetBtn}>Reset</button>
+          <button className="view-saved-questions" onClick={handleViewSavedBtn}>⭐ Saved</button>
+        </div>
+      </div>
     </div>
   )
 }
