b5dca754027e5f1b4129e4cc064447954da1a2b4
kuanchiliao1 <kuanchiliao@gmail.com>
2024-03-25 09:58:29 -0700

    Refactor to use logic from modelMessages.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
-const modelMessages = require('./modelMessages')
+const modelMessages = require('./modelMessages');
 const express = require('express');
+const cors = require('cors');
+const { parseQuiz, shuffle, decodeBase64 } = require('./utils');
+const { parse } = require('dotenv');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+console.log('express is running');
 
 async function fetchOpenAIOutput(messages, model, max_tokens) {
   const endpoint = 'https://api.openai.com/v1/chat/completions';
@@ -38,10 +44,19 @@ async function fetchOpenAIOutput(messages, model, max_tokens) {
   }
 }
 
-app.post('/api/generate-custom-quiz', async (req, res) => {
+app.post('/api/generate-score-feedback', async (req, res) => {
+  const {currentScore, totalScore} = req.body;
+  const message = modelMessages.scoreFeedback(currentScore, totalScore);
+  const response = await fetchOpenAIOutput(message, 'gpt-3.5-turbo', 70);
+  console.log(response);
   /*
     - Call the fetchOpenAIOutput function
-      - pass in object from request (messages, model, tokens)
+      - pass in info from request
+    Request object
+    {
+      currentScore: number,
+      totalScore: number
+    }
     - Return the output string
     - Maybe parse it
 
@@ -52,23 +67,32 @@ app.post('/api/generate-custom-quiz', async (req, res) => {
   */
 })
 
-app.get('/api/generate-score-feedback', async (req, res) => {
+app.get('/', (req, res) => console.log('test'))
+
+app.post('/api/generate-custom-quiz', async (req, res) => {
   /*
-  req format
+  requestData:
   {
     topic: string,
   }
-  requestData
 
-  modelMessages(requestData.topic)
+  messages = modelMessages(requestData.topic)
 
   req.body = {messages, model, max_tokens}
   */
-  const requestData = req.body;
-  const messages = modelMessages(requestData.topic);
-  const response = await fetchOpenAIOutput(messages, 'gpt-4-turbo-preview', 500);
-  console.log(response);
+  console.log({body: req.body}, 'req.body.topic: ', req.body.topic)
+  const {topic} = req.body;
+  const messages = modelMessages.customQuiz(topic);
+  const outputString = await fetchOpenAIOutput(messages, 'gpt-4-turbo-preview', 500);
+  console.log(parseQuiz(outputString));
+  res.json(parseQuiz(outputString));
 })
 
-const PORT = 3005;
+//TODO
+app.get('/api/opendb', (req, res) => {
+
+})
+
+/* TODO: get ports to auto update */
+const PORT = 3006;
 app.listen(PORT);
